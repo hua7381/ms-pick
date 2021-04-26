889101392e6d0ad99ea336671f45aba62de04a45
hua7381 <hua7381@foxmail.com>
2021-04-26 16:19:48 +0800

    readFileFromClasspath

--- src/main/java/com/hnkc/recognize/other/Tool.java
+++ src/main/java/com/hnkc/recognize/other/Tool.java
@@ -1,8 +1,8 @@
 package com.hnkc.recognize.other;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 工具类
@@ -13,18 +13,19 @@ import java.io.IOException;
 public class Tool {
 
     public static String readFileFromClasspath(String path) {
-        BufferedReader r = null;
         StringBuffer sb = new StringBuffer();
+        BufferedReader r = null;
         try {
-            r = new BufferedReader(new FileReader(getClasspath() + path));
+            r = new BufferedReader(
+                    new InputStreamReader(Tool.class.getClassLoader().getResourceAsStream(path), "utf-8"));
             String line = null;
             while ((line = r.readLine()) != null) {
-                if(sb.length() > 0) {
+                if (sb.length() > 0) {
                     sb.append("\n");
                 }
                 sb.append(line);
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
@@ -34,10 +35,6 @@ public class Tool {
             }
         }
         return sb.toString();
-    }
-
-    public static String getClasspath() {
-        return Thread.currentThread().getContextClassLoader().getResource("").getPath();
     }
 
 }
