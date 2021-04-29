64f6cd5a06a2f3a26b221e109020eca96d99838d
hua7381 <hua7381@foxmail.com>
2021-04-29 13:09:25 +0800

    小改swagger

--- src/main/java/com/hnkc/recognize/frame/config/SwaggerConfig.java
+++ src/main/java/com/hnkc/recognize/frame/config/SwaggerConfig.java
@@ -8,13 +8,13 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * swagger配置
+ * 
  * @author zhangguihua
  * @date 2020/09/29
  */
@@ -25,26 +25,20 @@ public class SwaggerConfig {
 
     @Bean
     public Docket docket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .apiInfo(apiInfo())
-            .select()
-            // 当前包路径
-            .apis(RequestHandlerSelectors.basePackage("com.hnkc.recognize.controller"))
-            .paths(PathSelectors.any())
-            .build();
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                // 当前包路径
+                .apis(RequestHandlerSelectors.basePackage("com.hnkc.recognize.controller")).paths(PathSelectors.any())
+                .build();
     }
 
     /**
      * 构建api文档的详细信息函数
+     * 
      * @return
      */
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-            .title("kc-微服务-识别与要素提取")
-            // 创建人
-            .contact(new Contact("zgh", "", ""))
-            .version("1.001")
-            .description("").build();
+        return new ApiInfoBuilder().title("KC-微服务-文本要素提取").version("1.001")
+                .description("维护: zhangguihua (hua7381@foxmail.com)").build();
     }
 
 }
