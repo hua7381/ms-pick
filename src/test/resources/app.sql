712bb916d2ca4082b6339873380162d0aef4fc43
zhangguihua <hua7381@foxmail.com>
2021-07-15 17:16:37 +0800

    1

--- src/test/resources/app.sql
+++ src/test/resources/app.sql
@@ -18,18 +18,19 @@ begin
 delete from t_recognize_reg where 1 = 1;
 insert into t_recognize_reg (keyword, contains, excludes) values ('首饰', '项链, 戒指', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('现金', '现金, 钱, 元被盗', '');
+insert into t_recognize_reg (keyword, contains, excludes) values ('平板电脑', '平板', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('宠物', '犬只, 猫只', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('家畜', '鸡, 鸭, 猪, 牛, 羊', '');
+
 insert into t_recognize_reg (keyword, contains, excludes) values ('盗窃', '门锁被撬, 撬锁, 偷, 盗', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('走失', '走失, 离家出走, 失联, 小孩@不见', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('诈骗', '骗, 冒充, 转账, 盗刷', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('打架', '打架, 斗殴, 殴打, 被打, 被人打, 打人, 肢体冲突', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('网贷', '网贷, 网络贷款', '');
-insert into t_recognize_reg (keyword, contains, excludes) values ('涉赌', '赌, 扑克, 麻将, 六合彩, 牌九', '');
+insert into t_recognize_reg (keyword, contains, excludes) values ('涉赌', '赌博, 扑克, 麻将, 六合彩, 牌九', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('涉黄', '涉黄, 色情服务, 卖淫, 嫖娼, 站街女, 妓, 特殊服务', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('涉毒', '涉毒, 吸毒, 毒品, 毒瘾, 戒毒, 海洛因, 冰毒, 摇头丸, 大麻', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('强奸', '强奸, 强行发生@性关系, 性侵', '');
-insert into t_recognize_reg (keyword, contains, excludes) values ('疫情', '疫情, 新冠肺, 病毒', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('噪音', '噪音, 声@扰民', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('堵车', '车@堵, 车@塞, 车@出入, 交通堵塞', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('乱停乱放', '乱停乱放, 乱停', '');
@@ -37,8 +38,10 @@ insert into t_recognize_reg (keyword, contains, excludes) values ('轻生', '自
 insert into t_recognize_reg (keyword, contains, excludes) values ('涉访', '涉访, 上访', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('火灾', '火灾, 起火, 火势, 着火', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('遗失', '遗留, 遗失', '');
-insert into t_recognize_reg (keyword, contains, excludes) values ('老人', '老人, 老年, 老年人', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('撞车', '车@撞, 车@碰', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('受伤', '伤, 出血', '');
 insert into t_recognize_reg (keyword, contains, excludes) values ('醉酒', '醉酒, 喝醉', '');
+
+insert into t_recognize_reg (keyword, contains, excludes) values ('老年人', '老人, 老年, 老年人', '');
+insert into t_recognize_reg (keyword, contains, excludes) values ('疫情', '疫情, 新冠肺, 病毒', '');
 end;
